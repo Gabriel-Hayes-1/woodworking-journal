a53ac8b9ec788091e8c2090505bca54df56f8b98
Gabriel <gabriel.hayes@outlook.com>
2024-11-17 21:23:31 -0500

    started work on Calendar

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
+const possibleIds = [
+    "home",
+    "bill-of-materials",
+    "equipment",
+    "calendar"
+]
+    
+
+
+
 function extractContent(id) {
-    const iframe = document.getElementById(id);
-    let iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-    let container = document.getElementById("container")
-    container.innerHTML = iframeDoc.getElementById("to-be-cloned").innerHTML
+    if (possibleIds.includes(id)) {
+        const iframe = document.getElementById(id);
+        let iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+        let container = document.getElementById("container")
+        container.innerHTML = iframeDoc.getElementById("to-be-cloned").innerHTML
+
+
+        let elements = document.getElementsByClassName("selected-site")
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.remove("selected-site")
+        }
+
+        document.getElementById(possibleIds.indexOf(id)).classList.add("selected-site")
+        
+    } else {alert("id inputted does not exsist")}
+
+    
+
+
 }
-function home() {
-    document.getElementById("container").innerHTML = ""
-}
+document.onload
 
 
 
