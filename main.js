d16d81371fbcda26b57cffc7f1085cb227320620
Goldenknight3 <gabriel.hayes@outlook.com>
2024-11-17 19:52:10 -0500

    made changes, but they dont work.

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const iframe = document.getElementById('bill-of-materials');
+function billOfMats() {
+    alert("hi")
+    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    document.getElementById("container").innertext = iframe.contentDocument.getElementById("to-be-cloned").innerHTML
+}
+function home() {
+    alert("bye");
+    document.getElementById("container").innerHTML = ""
+}
+
+
+
 
