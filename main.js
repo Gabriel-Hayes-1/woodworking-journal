e5beeb416390757aaeb920f3e30da34090aed8ea
Gabriel <gabriel.hayes@outlook.com>
2024-11-17 20:35:33 -0500

    fixed bugs and added rest of files

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const iframe = document.getElementById('bill-of-materials');
-function billOfMats() {
-    alert("hi")
-    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-    document.getElementById("container").innertext = iframe.contentDocument.getElementById("to-be-cloned").innerHTML
+function extractContent(id) {
+    const iframe = document.getElementById(id);
+    let iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    let container = document.getElementById("container")
+    container.innerHTML = iframeDoc.getElementById("to-be-cloned").innerHTML
 }
 function home() {
-    alert("bye");
     document.getElementById("container").innerHTML = ""
 }
 
