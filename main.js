cc67a861a5a3281322aecad6897c4de9a10de89b
Gabriel <gabriel.hayes@outlook.com>
2024-11-18 19:46:01 -0500

    fixed nav issues

--- main.js
+++ main.js
@@ -3,32 +3,24 @@ const possibleIds = [
     "bill-of-materials",
     "equipment",
     "calendar"
-]
-    
-
-
+];
+const container = document.getElementById("container")
 
 function extractContent(id) {
-    if (possibleIds.includes(id)) {
-        const iframe = document.getElementById(id);
-        let iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-        let container = document.getElementById("container")
-        container.innerHTML = iframeDoc.getElementById("to-be-cloned").innerHTML
-
-
-        let elements = document.getElementsByClassName("selected-site")
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].classList.remove("selected-site")
+        let template = document.getElementById(id)
+        if (template) {
+            container.innerHTML = template.innerHTML
+        } else {
+            alert(`template for ${id} does not exsist`)
         }
-
-        document.getElementById(possibleIds.indexOf(id)).classList.add("selected-site")
-        
-    } else {alert("id inputted does not exsist")}
-
+        const elements = document.getElementsByClassName("selected-site");
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.remove("selected-site");
+        }
+        document.getElementById(possibleIds.indexOf(id)).classList.add("selected-site");
     
-
-
 }
+
 document.onload
 
 
